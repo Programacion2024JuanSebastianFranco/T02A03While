a70a7fc27432ac10f911808bee0bdc426845d551
Franco <juafracas@alu.edu.gva.es>
2024-10-15 19:11:16 +0200

    empezando ejercicio.. 13??

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.awt.print.Printable;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+
+        //8
+        int contadorInicio;
+        int contadorFin;
+
+        System.out.println("Introduce el numero de inicio: ");
+        contadorInicio = scan.nextInt();
+        System.out.println("Introduce el numero final: ");
+        contadorFin = scan.nextInt();
+
+        while (contadorInicio <= contadorFin) {
+            System.out.println("contador es: "+contadorInicio);
+            contadorInicio++;
+        }
+        System.out.println("Termina el bucle");
+
+
+        //
+
     }
 }
